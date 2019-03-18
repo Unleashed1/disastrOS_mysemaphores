d819d6ec75b65964b5e2a0b666556aea930cc77f
Unleashed1 <mastrogiacomo.1748008@studenti.uniroma1.it>
2019-03-18 18:29:13 +0100

    realizzazione sem_close tested

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,34 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+  //First of all take the fd of the semDescriptor that we wants to close(check the list)
+  
+  
+  int myfd = running->syscall_args[0];
+  
+  SemDescriptor* sem_desc = semDescriptorList_byFd(&running->sem_descriptors,myfd);
+  //Need to be sure 
+  if(!sem_desc){
+	  running->syscall_retvalue = DSOS_ESEMCLOSE_SEMD_NOT_IN_PROCESS;
+	  return;
+  }
+  
+  //Now remove it from the list, remove the ptr fom the list of descriptors && free resources
+
+  List_detach(&running->sem_descriptors, (ListItem*)sem_desc);
+  Semaphore* sem = sem_desc->semaphore;
+  assert(sem);
+  SemDescriptor_free(sem_desc);
+  SemDescriptorPtr* sem_desc_ptr = (SemDescriptorPtr*)List_detach(&sem->descriptors, (ListItem*)(sem_desc->ptr));
+  assert(sem_desc_ptr);
+  SemDescriptorPtr_free(sem_desc_ptr);
+  
+  //Before return the syscall success(0),check if there are another process that could be on sem
+  if(sem->descriptors.size==0) {
+	  //cast
+	  sem = (Semaphore*)List_detach(&semaphores_list,(ListItem*)sem;
+	  assert(sem);
+	  Semaphore_free(sem);
+  }
+  running->syscall_retvalue = 0;
 }
